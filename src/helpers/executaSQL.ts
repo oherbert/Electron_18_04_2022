1290629ec9ae6f60ac3fabf32c89a3394a7c0b38
oherbert <o_herbert@hotmail.com>
2022-05-23 21:19:46 -0300

    tratamentos de CLOBs no execSql

--- src/helpers/executaSQL.ts
+++ src/helpers/executaSQL.ts
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import fs from 'fs';
 import oracledb, { BindParameters, ExecuteOptions } from 'oracledb';
 import getDBConfig from '../config/database';
 import MainEnv from '../main/MainEnv';
 
-oracledb.fetchAsString = [oracledb.DATE /* , oracledb.NUMBER */];
+oracledb.fetchAsString = [oracledb.DATE, oracledb.CLOB, oracledb.NCLOB];
+oracledb.fetchAsBuffer = [oracledb.BLOB];
 oracledb.autoCommit = true;
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
@@ -39,10 +41,32 @@ const reloadDBConfig = () => {
 
 setAppInstantClient();
 
+const blobsToBase64 = (
+  rows: any[] | undefined,
+  metaData: oracledb.Metadata<any>[] | undefined
+) => {
+  if (metaData && rows && metaData.length > 0) {
+    const blobsFields = metaData
+      .map((data) => (data.dbTypeName === 'BLOB' ? data.name : false))
+      .filter((f) => f);
+
+    if (blobsFields) {
+      rows.forEach((row: any) => {
+        Object.keys(row).forEach((key) => {
+          if (blobsFields.includes(key)) {
+            row[key] = row[key].toString('base64');
+          }
+        });
+      });
+    }
+  }
+  return rows;
+};
+
 export default async function execSql(
   statement: string,
   binds: BindParameters = [],
-  options: ExecuteOptions = {}
+  options: ExecuteOptions = { extendedMetaData: true }
 ) {
   reloadDBConfig();
   // console.log(oracledb.oracleClientVersion);
@@ -53,11 +77,50 @@ export default async function execSql(
     pool = await oracledb.createPool(dbConfig);
 
     let connection;
+    let opt = null;
+
     try {
       connection = await pool.getConnection();
-      const result = await connection.execute(statement, binds, options);
 
-      return result.rows;
+      const stm = statement.toLowerCase();
+
+      if (stm.indexOf('begin') > -1 && stm.indexOf('end') > -1) {
+        if (stm.indexOf(':cursor') > -1) {
+          opt = {
+            cursor: {
+              type: oracledb.CURSOR,
+              dir: oracledb.BIND_OUT,
+            },
+          };
+        } else if (stm.indexOf(':retorno') > -1) {
+          opt = {
+            retorno: {
+              type: oracledb.CURSOR,
+              dir: oracledb.BIND_OUT,
+            },
+          };
+        }
+      }
+
+      const result = opt
+        ? await connection.execute<any>(statement, opt, options)
+        : await connection.execute<any>(statement, binds, options);
+
+      const resultSet = result.outBinds
+        ? result.outBinds.cursor ?? result.outBinds.retorno
+        : null;
+
+      // Tratamento de cursores
+      if (resultSet) {
+        let rows: any[] | undefined = await resultSet.getRows();
+        rows = blobsToBase64(rows, resultSet.metaData);
+
+        await resultSet.close();
+
+        return rows;
+      }
+
+      return blobsToBase64(result.rows, result.metaData);
     } catch (err: any) {
       console.log(err);
 
@@ -65,7 +128,7 @@ export default async function execSql(
     } finally {
       if (connection) {
         try {
-          await connection.close(); // Put the connection back in the pool
+          await connection.close();
         } catch (err) {
           console.log(err);
         }
